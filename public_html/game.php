dc1a5ec9eb88a11ce48536acf1a122aefe2b7ac2
georgeCherry02 <georgeb.cherry@gmail.com>
2020-05-11 13:20:14 +0100

    Added a mobile to desktop redirect message
    Simple message that only appears on mobile devices

--- public_html/game.php
+++ public_html/game.php
@@ -7,5 +7,12 @@
     $page_title = "Game";
     include_once "../inc/components/header.php";
     include_once "../inc/components/navbar.php";
+
+?>
+<div id="desktop_optimisation_message" class="container text-center d-flex flex-column justify-content-center d-lg-none">
+    <h5 class="grey-text">Hi, this game is optimised for use on a desktop computer!</h5>
+    <p class="grey-text">Please switch over to desktop to play the game and help our research!</p>
+</div>
+<?php
     include_once "../inc/components/footer.php";
 ?>
