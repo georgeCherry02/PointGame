09ebe8b4caf8924d3526ccb8f3219de5559b2c34
georgeCherry02 <georgeb.cherry@gmail.com>
2020-05-19 13:02:53 +0100

    game.js required a complete rewrite
    Using a compound shape was proving unreliable and bugs kept cropping up.
    Now rely on caching and division to loop through each individual point path.
    
    This simplifies point removal and addition.
    
    General structure of the program is largely similar, the main change is how it validates a location

--- public_html/scripts/game.js
+++ public_html/scripts/game.js
@@ -1,228 +1,224 @@
-// Define properties of canvas
-var game_canvas = $("#game_canvas");
-var origin = new paper.Point(0, 0);
-var canvas_size = new paper.Size(1024, 1024);
-
-// Define colours used
+// Define external constants
+const MIN_RADIUS = 10;
+const MAX_RADIUS = 10;
 const accept_colour = new paper.Color(0.9, 1, 0.9, 1);
 const reject_colour = new paper.Color(1, 0.9, 0.9, 1);
 const point_colour = new paper.Color(0.9, 0.9, 1, 1);
 
-// Define list of points
-var point_images_list = [];
-var point_areas_list = [];
+with (paper) {
+    var game = {};
 
-// Define global paths
-var accept_point_region;
-var canvas_region;
+    // Define basic properties
+    game.canvas = $("#game_canvas");
+    game.origin = new Point(0, 0);
+    game.canvas_size = new Size(1024, 1024);
+    game.total_point_number = 0;
+    
+    // Define list of points
+    game.point_images_list = {};
+    game.point_areas_list = {};
 
-const min_radius = 10;
-const max_radius = 20;
+    // Divide canvas into quadrants
+    // 0 1 ... 8
+    // 9 ...
+    // 17 ...
+    game.canvas_sections = [];
+    for (var i = 0; i < 64; i++) {
+        game.canvas_sections.push([]);
+    }
+    // Define the variables required for caching sections
+    game.last_used_section_requires_update = false;
+    game.last_used_section_and_surroundings_id;
+    game.last_used_section_and_surroundings = [];
 
-window.onload = function() {
-    with(paper) {
-        Logger.log(LoggingType.STATUS, "Initialising canvas");
-        setup(game_canvas[0]);
+    // Define which mode the game's in
+    game.chaining = MAX_RADIUS > MIN_RADIUS;
 
-        // Define layers
-        var base_layer = new Layer();
-        var point_areas_layer = new Layer();
-        var points_layer = new Layer();
-        var mouse_track_layer = new Layer();
-        mouse_track_layer.opacity = 0.5;
+    game.init = function() {
+        Logger.log(LoggingType.STATUS, "Initialising Canvas");
+        setup(this.canvas[0]);
 
-        // Define acceptance region and the canvas in a shape
-        base_layer.activate();
-        accept_point_region = new Path.Rectangle(origin, canvas_size);
-        canvas_region = new Path.Rectangle(origin, canvas_size);
-        view.draw();
-
-        // Define the mouse marker and it's surroundings
-        // These get moved around to follow the mouse
-        mouse_track_layer.activate();
-        const validation_indicator = new Path.Circle({
+        this.base_layer = new Layer();
+        this.point_areas_layer = new Layer();
+        this.points_layer = new Layer();
+        this.mouse_track_layer = new Layer();
+    
+        // Initialise custom mouse
+        this.mouse_track_layer.opacity = 0.5;
+        this.mouse_validity_indicator = new Path.Circle({
             center: new Point(-15, -15),
-            radius: 15
+            radius: 10
         });
-        const mouse_marker = new Path.Circle({
+        this.mouse_marker = new Path.Circle({
             center: new Point(-1, -1),
-            radius: 2
+            radius: 1
         });
-
-        // Initialise the Tool that runs the game
-        // This picks up on all mouse events occuring on the canvas and handles appropriately
-        var game_tool = new Tool();
-        game_tool.onMouseDown = function(event) {
-            var location = event.point;
-            if (accept_point_region.contains(location)) {
-                if (max_radius > min_radius) {
-                    // Appropriately sets APR if no points have been placed down
-                    if (point_images_list.length === 0) {
-                        // Activate appropriate layer
-                        base_layer.activate();
-                        // Remove all APR
-                        accept_point_region = new Path.Circle({
-                            center: new Point(-1, -1),
-                            radius: 1
-                        });
-                        accept_point_region.strokeColor = "lightgrey";
-                    }
-                    // Check if any previous points have been effected
-                    var effected_point_indices = [];
-                    for (var i = 0; i < point_images_list.length; i++) {
-                        if (point_images_list[i].position.getDistance(location) < 20) {
-                            effected_point_indices.push(i);
-                        }
-                    }
-                    // Add the max_radius circle
-                    updateAcceptPointRegion(location, false, max_radius);
-                    // Re-remove all the old points
-                    var c_path;
-                    for (var i = 0; i < effected_point_indices.length; i++) {
-                        // Get the point
-                        c_path = point_images_list[effected_point_indices[i]];
-                        // Remove the area from the apr
-                        updateAcceptPointRegion(c_path.position);
-                    }
-                }
-                // Removes point from clickable region
-                updateAcceptPointRegion(location);
-                // Renders the point on screen
-                renderPoint(location);
+    
+        // Initialise game_tool
+        this.point_tool = new Tool();
+        this.point_tool.onMouseDown = function(event) {
+            game.last_used_section_requires_update = true;
+            if (game.checkValidity(event.point)) {
+                // Activate appropriate layer
+                game.points_layer.activate();
+                // Draw point onto canvas
+                game.renderPoint(event.point);
             } else {
-                // If the point is not contained in the accept points region loop through paths and remove appropriate
-                effected_point_indices = [];
-                for (var i = 0; i < point_areas_list.length; i++) {
-                    // If the point contains the location of the event
-                    if (point_areas_list[i].contains(location)) {
-                        // Push to effected points
-                        effected_point_indices.push(i);
-                    }
-                }
-                // Determine the closest point to the event
-                var closest_point_index = effected_point_indices[0];
-                var old_point = point_areas_list[effected_point_indices[0]];
-                var distance_from_old_to_event = old_point.position.getDistance(location);
-                var new_point, distance_from_new_to_event;
-                for (var i = 1; i < effected_point_indices.length; i++) {
-                    new_point = point_areas_list[effected_point_indices[i]];
-                    distance_from_new_to_event = new_point.position.getDistance(location);
-                    if (distance_from_new_to_event < distance_from_old_to_event) {
-                        distance_from_old_to_event = distance_from_new_to_event;
-                        closest_point_index = effected_point_indices[i];
-                    }
-                }
-
-                // Deal with the removal of a point
-                removePoint(effected_point_indices, closest_point_index);
+                // Fetch closest point
+                var nearest_point_id = game.fetchNearestPoint(event.point);
+                var nearest_point = game.point_areas_list[nearest_point_id];
+                // Remove this point from tracked lists...
+                // Determine the section we're dealing with
+                var section_id = game.determineSectionID(nearest_point.position);
+                // Remove point_area path from section
+                var list_of_ids = game.canvas_sections[section_id];
+                list_of_ids.splice(list_of_ids.indexOf(nearest_point_id), 1);
+                game.canvas_sections[section_id] = list_of_ids;
+                // Remove point_area from tracking list and rendering
+                game.point_areas_layer.activate();
+                nearest_point.remove();
+                delete game.point_areas_list[nearest_point_id];
+                // Remove point_image from tracking list and rendering
+                var nearest_point_image = game.point_images_list[nearest_point_id];
+                game.points_layer.activate();
+                nearest_point_image.remove();
+                delete game.point_images_list[nearest_point_id];
+                // Remove both point_area and point_image from rendering
             }
-            // Updates mouse appearance after point is placed (should change colour to red as it's no longer in acceptance region)
-            updateMouseAppearance(event.point);
-            // Redraws the canvas
+            // After effect of click will always require update
+            game.last_used_section_requires_update = true;
+            // Update the mouse appearance after placing point
+            game.updateMouseAppearance(event.point);
+            // Render the game
             view.draw();
         }
-        game_tool.onMouseMove = function(event) {
-            // Updates the mouse appearance on move so that the mimic mouse follows the actual cursor
-            updateMouseAppearance(event.point);
-            // Redraws the canvas
+
+        this.point_tool.onMouseMove = function(event) {
+            // Update the mouse appearance on each move
+            game.updateMouseAppearance(event.point);
+            // Render the game
             view.draw();
-            // Prevents touch scrolling incase somebody uses a touch screen device for this
-            return false;
-        }
-        // Activates the game tool
-        game_tool.activate();
-
-        // Moves around the fake mouse that allows indication of whether or not you can place a point
-        function updateMouseAppearance(location) {
-            // Activates the appropriate layer
-            mouse_track_layer.activate();
-
-            // Checks if the event occured as the mouse was leaving
-            if (!canvas_region.contains(location)) {
-                // If so move mouse off canvas
-                validation_indicator.position = new Point(-15, -15);
-                mouse_marker.position = new Point(-1, -1);
-                return;
-            }
-
-            // Move mouse_marker and validation_indicator to event (cursor) position
-            validation_indicator.position = location;
-            mouse_marker.position = location;
-
-            // Pick the appropriate colour, i.e. whether a click will be accepted or not
-            validation_indicator.fillColor = accept_colour;
-            mouse_marker.fillColor = "green";
-            if (!accept_point_region.contains(location)) {
-                validation_indicator.fillColor = reject_colour;
-                mouse_marker.fillColor = "red";
-            }
-        }
-
-        var outer;
-        // Removes points appropriately from acceptance region
-        function updateAcceptPointRegion(location, remove = true, radius = min_radius) {
-            // Activates the appropriate layer
-            base_layer.activate();
-            // Defines the area to remove from region
-            outer = new Path.Circle({
-                center: location,
-                radius: radius 
-            });
-            // Adds the path to the points list
-            // Removes the region from the acceptance region
-            if (remove) {
-                accept_point_region = accept_point_region.subtract(outer);
-            } else {
-                accept_point_region = accept_point_region.unite(outer);
-            }
-        }
-
-        var colour_point, point_area;
-        // Renders a new point at given location
-        function renderPoint(location) {
-            // Activates the appropriate layer
-            point_areas_layer.activate();
-            // Defines the area to colour in
-            point_area = new Path.Circle({
-                center: location,
-                radius: min_radius
-            });
-            point_area.fillColor = point_colour;
-            points_layer.activate();
-            colour_point = new Path.Circle({
-                center: location,
-                radius: 2 
-            });
-            colour_point.fillColor = "blue";
-            // Pushes path to list
-            point_images_list.push(colour_point);
-            point_areas_list.push(point_area);
-        }
-
-        function removePoint(effected_point_indices, removed_point_index) {
-            // Remove the point from APR
-            updateAcceptPointRegion(point_areas_list[removed_point_index].position, false);
-            points_layer.activate();
-            // Remove point dot
-            point_images_list[removed_point_index].remove();
-            point_images_list.splice(removed_point_index, 1);
-            // Remove point surrounding
-            point_areas_list[removed_point_index].remove();
-            point_areas_list.splice(removed_point_index, 1);
-
-            // Re introduce APR for all the other points
-            var current_point_index;
-            for (var i = 0; i < effected_point_indices.length; i++) {
-                current_point_index = effected_point_indices[i];
-                if (current_point_index !== removed_point_index) {
-                    // Account for array shift from splice
-                    if (current_point_index > removed_point_index) {
-                        current_point_index--;
-                    }
-                    // Make sure that each effected point keeps it's APR
-                    updateAcceptPointRegion(point_areas_list[current_point_index].position);
-                }
-            }
         }
     }
+
+    game.checkValidity = function(location) {
+        // Activate appropriate layer
+        this.point_areas_layer.activate();
+
+        // Check which quadrant the mouse is in
+        var section_to_examine = this.determineSectionAndSurroundings(location);
+
+        // Loop through all points in the section and it's surroundings (for edge cases) looking for collision
+        for (var i = 0; i < section_to_examine.length; i++) {
+            var index = section_to_examine[i];
+            if (this.point_areas_list[index].contains(location)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    game.determineSection = function(location) {
+        // Divided into 64 sections
+        var section_number = this.determineSectionID(location);
+        if (section_number < 0 || section_number > 63) {
+            return [];
+        }
+        return this.canvas_sections[section_number];
+    }
+    game.determineSectionAndSurroundings = function(location) {
+        var i = Math.floor((location.x / 1024) * 8);
+        var j = Math.floor((location.y / 1024) * 8);
+        var section_id = this.determineSectionID(location);
+        // Trying to optimise a bit by checking if we've already formed this array
+        if (!this.last_used_section_requires_update && section_id == this.last_used_section_and_surroundings_id) {
+            return this.last_used_section_and_surroundings;
+        }
+        this.last_used_section_and_surroundings_id = section_id;
+        this.last_used_section_requires_update = false;
+        var resulting_array = [];
+        for (var section_i = i - 1; section_i <= i + 1; section_i++) {
+            if (section_i >= 0 && section_i < 8) {
+                for (var section_j = j - 1; section_j <= j + 1; section_j++) {
+                    if (section_j >= 0 && section_j < 8) {
+                        var section_number = 8 * section_j + section_i;
+                        Array.prototype.push.apply(resulting_array, this.canvas_sections[section_number]);
+                    }
+                }       
+            }
+        }
+        this.last_used_section_and_surroundings = resulting_array;
+        return resulting_array;
+    }
+    game.determineSectionID = function(location) {
+        var i = Math.floor((location.x / 1024) * 8);
+        var j = Math.floor((location.y / 1024) * 8);
+        return 8*j + i;
+    }
+    game.fetchNearestPoint = function(location) {
+        // Fetch appropriate sections
+        var area_to_examine = this.determineSectionAndSurroundings(location);
+        var closest_point_id, current_point, shortest_distance;
+        for (var i = 0; i < area_to_examine.length; i++) {
+            current_point = this.point_areas_list[area_to_examine[i]];
+            if (current_point.contains(location)) {
+                // Determine if a closest point has been set or if the current point is nearer
+                if (typeof closest_point_id === "undefined" || location.getDistance(current_point.position) < shortest_distance) {
+                    closest_point_id = area_to_examine[i];
+                    shortest_distance = location.getDistance(current_point.position);
+                } 
+            }
+        }
+        return closest_point_id;
+    }
+    game.renderPoint = function(location) {
+        // Activate appropriate layer
+        this.point_areas_layer.activate();
+        // Define the point's area appearance
+        var point_area = new Path.Circle({
+            center: location,
+            radius: MIN_RADIUS
+        });
+        point_area.fillColor = point_colour;
+        // Push to own list to keep track of each point
+        this.point_areas_list[this.total_point_number] = point_area;
+        // Do same again for actual point
+        this.points_layer.activate();
+        var point_image = new Path.Circle({
+            center: location,
+            radius: 1
+        });
+        point_image.fillColor = "blue";
+        this.point_images_list[this.total_point_number] = point_image;
+
+        // Push to index tracking quadrants
+        this.determineSection(location).push(this.total_point_number);
+        this.total_point_number++;
+    }
+    game.updateMouseAppearance = function(location) {
+        this.mouse_track_layer.activate();
+
+        // Move custom mouse to correct location
+        this.mouse_marker.position = location;
+        this.mouse_validity_indicator.position = location;
+
+        // Correct custom mouse colour
+        // Have used if and else because fill operation seems to take a long time and only want to call it once if possible
+        if (!game.checkValidity(location)) {
+            this.mouse_marker.fillColor = "red";
+            this.mouse_validity_indicator.fillColor = reject_colour;
+        } else {
+            this.mouse_marker.fillColor = "green";
+            this.mouse_validity_indicator.fillColor = accept_colour;
+        }
+    }
+}
+
+window.onload = function() {
+    game.init();
+    // for (var i = 0; i < 512; i++) {
+    //     var x_loc = 1024 * Math.random();
+    //     var y_loc = 1024 * Math.random();
+    //     var location = new paper.Point(x_loc, y_loc);
+    //     game.renderPoint(location);
+    // }
 }
