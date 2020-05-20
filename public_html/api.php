664a37384298aad13cc8ba230d12477084479dc4
georgeCherry02 <georgeb.cherry@gmail.com>
2020-05-20 16:39:59 +0100

    Implemented outline of backend point submission
    Included an API process with basic documentation on bottom of file to be rewritten elsewhere
    
    So far backend checks that request is of correct form yet does not do anything with data

--- public_html/api.php
+++ public_html/api.php
@@ -28,7 +28,60 @@
     // Validate provided process
     // Error codes documented at bottom of API
     switch($_POST["process"]) {
+        case "submitPoints":
+            $response["process"] = "submitPoints";
+            include_once "../inc/enums/Shapes.php";
+            // Decode the data
+            $request_data = json_decode($_POST["data"], $assoc=TRUE);
+            // Check the data decoded correctly
+            if (sizeof($request_data) === 0) {
+                $response["error_message"] = "Malformed Data";
+                $response["error_code"] = 1;
+                break;
+            }
+            // Check the shape matches the expected shape
+            if ($request_data["expected_shape"] != $_SESSION["Expected_Shape_ID"]) {
+                $response["error_message"] = "Unexpected shape tag received";
+                $response["error_code"] = 2;
+                break;
+            }
+            // Check their are the correct number of points
+            if (sizeof($request_data["point_pattern"]) < $_SESSION["min_points_number"]) {
+                $response["error_message"] = "Too few points";
+                $response["error_code"] = 3;
+                break;
+            }
+            if (sizeof($request_data["point_pattern"]) > $_SESSION["max_points_number"]) {
+                $response["error_message"] = "Too many points";
+                $response["error_code"] = 4;
+                break;
+            }
+            // ##########################################################################################
+            // # Insert into database
+            // ##########################################################################################
+            $response["status"] = "success";
+            break;
         default:
             $response["error_message"] = "Failed to provide valid process";
     }
     echo json_encode($response);
+/*  Documentation
+    Processes:
+        submitPoints:
+            Method:
+                POST
+            Expected Datatype:
+                JSON
+            Parameters:
+                expected_shape - A valid shape ID
+                limitations - A JSON object representing the limitations implemented when drawing the shape
+                point_pattern - A list of all the coordinates of the points
+            Description:
+                This method should be used for inserting a new point pattern into the database
+            Error Codes:
+                1 - Data wasn't in valid JSON format    
+                2 - The shape tag received didn't match the expected tag
+                3 - There were too few points submitted
+                4 - There were too many points submitted
+*/
+?>
