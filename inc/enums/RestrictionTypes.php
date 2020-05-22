a9c15cbae018f7866d648eec35548e89197b827e
georgeCherry02 <georgeb.cherry@gmail.com>
2020-05-22 21:04:46 +0100

    Implemented a render method for form groups in restriction type enum
    Generalised the method to render form-groups for restriction types

--- inc/enums/RestrictionTypes.php
+++ inc/enums/RestrictionTypes.php
@@ -15,5 +15,40 @@
         public function getRenderName() {
             return str_replace("_", " ", $this->_getProperty("functional_name"));
         }
+
+        public function getFormGroupRender() {
+            $small_name = $this->getFunctionalName();
+            $pretty_name = $this->getRenderName();
+            $html = "<div class=\"form-group\">"
+                  .     "<input type=\"hidden\" id=\"".$small_name."_number\" name=\"".$small_name."_number\" value=\"1\">"
+                  .     "<div class=\"form-row\">"
+                  .         "<div class=\"col-10\">"
+                  .             "<label for=\"".$small_name."_group\">".$pretty_name." Distributions</label>"
+                  .         "</div>"
+                  .         "<div class=\"col-1\">"
+                  .             "<button id=\"".$small_name."_remove_button\" class=\"btn btn-danger btn-sm float-right\">"
+                  .                 "<i class=\"fas fa-minus\"></i>"
+                  .             "</button>"
+                  .         "</div>"
+                  .         "<div class=\"col-1\">"
+                  .             "<button id=\"".$small_name."_add_button\" class=\"btn btn-success btn-sm\">"
+                  .                 "<i class=\"fas fa-plus\"></i>"
+                  .             "</button>"
+                  .         "</div>"
+                  .     "</div>"
+                  .     "<div id=\"".$small_name."_group\">"
+                  .         "<div id=\"".$small_name."_1\" class=\"form-row\">"
+                  .             "<div class=\"col-8\">"
+                  .                 "<small id=\"".$small_name."_1_desc\" class=\"form-text text-muted ml-1\">".$pretty_name." 1</small>"
+                  .                 "<input type=\"number\" name=\"".$small_name."_1\" class=\"form-control\" placeholder=\"".$pretty_name." (px)\" aria-describedby=\"".$small_name."_1_desc\"/>"
+                  .             "</div>"
+                  .             "<div class=\"col-4\">"
+                  .                 "<input type=\"number\" name=\"".$small_name."_chance_1\" min=\"1\" max=\"100\" class=\"form-control percentage_input\" placeholder=\"Likelihood\"/>"
+                  .             "</div>"
+                  .         "</div>"
+                  .     "</div>"
+                  . "</div>";
+            return $html;
+        }
     }
 ?>
