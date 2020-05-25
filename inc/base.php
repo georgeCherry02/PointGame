24707c8e8fb41a28cbb8ff50b7a8239bec2031c7
georgeCherry02 <georgeb.cherry@gmail.com>
2020-05-25 15:59:52 +0100

    Corrected base.php for admin pages

--- inc/base.php
+++ inc/base.php
@@ -3,7 +3,7 @@
     error_reporting(E_ALL);
 
     // Include database constants
-    if ($admin) {
+    if (isset($admin) && $admin) {
         include_once "../../inc/db_constants.php";
     } else {
         include_once "../inc/db_constants.php";
