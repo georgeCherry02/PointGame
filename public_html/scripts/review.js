3b034ce26bd7128be7b38d89671d747a1072463f
georgeCherry02 <georgeb.cherry@gmail.com>
2020-06-07 13:44:12 +0100

    Added a function to populate review canvases with point patterns

--- public_html/scripts/review.js
+++ public_html/scripts/review.js
@@ -55,3 +55,42 @@ review.init = function() {
         }
     })
 }
+review.updateCanvases = function() {
+    Logger.log(LoggingType.STATUS, "Setting up canvases");
+    for (var i = 1; i <= this.review_amount; i++) {
+        // (current point pattern)
+        var cpp = this.point_patterns[i-1];
+        // Update question for canvas
+        $("#review_question_"+i).html(cpp["Shape_Name"]);
+        // Populate the ID input
+        $("#review_pattern_id_"+i).val(cpp["ID"]);
+        // Begin to update canvas
+        var current_scope = this.canvas_scopes[i-1];
+        current_scope.activate();
+        // Declare point colour
+        var point_colour = new paper.Color(0.9, 0.9, 1, 1);
+        // Select canvas
+        paper.setup($("#review_canvas_"+i)[0]);
+        // Create layers
+        var point_areas = new paper.Layer();
+        var point_images = new paper.Layer();
+        for (var j = 0; j < cpp.x.length; j++) {
+            // Note this canvas is half scale of the one it was drawn on hence the /2
+            // Add all point areas
+            point_areas.activate();
+            var point_area = new paper.Path.Circle({
+                center: new paper.Point(cpp.x[j]/2, cpp.y[j]/2),
+                radius: cpp["Minimum_Radius"]/2
+            });
+            point_area.fillColor = point_colour;
+            // Add all point images
+            point_images.activate();
+            var point_image = new paper.Path.Circle({
+                center: new paper.Point(cpp.x[j]/2, cpp.y[j]/2),
+                radius: 0.5
+            });
+            point_image.fillColor = "blue";
+            paper.view.draw();
+        }
+    }
+}
