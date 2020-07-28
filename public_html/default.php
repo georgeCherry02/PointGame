6ffd027ab92692b82cc04a1c89697d526363b25f
georgeCherry02 <georgeb.cherry@gmail.com>
2020-07-28 17:38:21 +0100

    Restyled landing page to be more modern

--- public_html/default.php
+++ public_html/default.php
@@ -2,20 +2,20 @@
     include_once "../inc/base.php";
 
     $page_title = "Home";
+    $page_description = "Landing page for the citizen science project Spotting Patterns!";
     include_once "../inc/components/header.php";
     include_once "../inc/components/navbar.php";
 ?>
-<div class="landing jumbotron white-background">
-    <div class="landing-circle-1"></div>
-    <div class="landing-circle-2"></div>
+<div class="landing-page-jumbotron">
+    <div id="hero_text_container" class="text-container">
+        <h1>Spotting Patterns</h1>
+        <h5 class="mt-4">A citizen science initiative</h5>
+    </div>
 </div>
-<div class="container">
-    <h4 class="grey-text">Hi, welcome to "What's The Point?"!</h4>
-    <p class="grey-text">We're a citizen science initiative looking to develop new mathematical techniques to automatically identify shape and structure in datasets of point patterns. This is something that humans can easily do, yet currently computers struggle to do accurately. Interpreting spatial patterns in these datasets is crucial for improving our understanding of problems in a huge range of areas, including describing the clustering of immune cells within tumours, the structure of neurons in the brain, and the way that information can be interpreted by computers more generally.</p>
-    <p class="grey-text">By playing our game you can help us form large datasets of labelled point patterns, in turn allowing us to develop new mathematical descriptions of point data with all the benefits as described above! So, play away!</p>
-    <a href="game.php">
-        <button type="button" class="btn btn-primary highlight-background highlight-border">Play!</button>
-    </a>
+<div class="play_button">
+    <div class="link_container">
+        <a href="game.php">Play Game</a>
+    </div>
 </div>
 <?php
     include_once "../inc/components/footer.php";
